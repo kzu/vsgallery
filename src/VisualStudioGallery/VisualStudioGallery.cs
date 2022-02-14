7a9412110a4f1a49f0ded6b2c210b8f12f758924
Daniel Cazzulino <daniel@cazzulino.com>
2022-02-14 18:51:49 -0300

    Remove unused namespaces

--- src/VisualStudioGallery/VisualStudioGallery.cs
+++ src/VisualStudioGallery/VisualStudioGallery.cs
@@ -2,8 +2,6 @@ using System;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml;
